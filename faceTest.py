2b6d21069646acd861a88bba97825d264dce91c6
Cody Machine <codymachine93@gmail.com>
2018-02-07 12:44:15 -0800

    faceTest Added
    Added a file for face detection. It is almost line for line the same as
    in the tutorial video, https://www.youtube.com/watch?v=88HdqNDQsEk

---
+++ faceTest.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+# Load in cascade files
+face_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_eye.xml')
+
+originalFeed = cv2.VideoCapture(0)
+
+if originalFeed.isOpened():  # try to get the first frame
+    rval, frame = originalFeed.read()
+else:
+    rval = False
+
+while rval:
+    rval, frame = originalFeed.read()
+
+    grayFeed = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    facesDetected = face_cascade.detectMultiScale(grayFeed, 1.3, 5)
+    for (x,y,w,h) in facesDetected:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+        roiGray = grayFeed[y:y+h, x:x+w]
+        roiColor = frame[y:y+h, x:x+w]
+        eyesDetected = eye_cascade.detectMultiScale(roiGray)
+        for (ex,ey,ew,eh) in eyesDetected:
+            cv2.rectangle(roiColor, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
+
+    cv2.imshow("preview", frame)
+
+
+    key = cv2.waitKey(20)
+    if key == 27:  # exit on ESC
+        break
+
+originalFeed.release()
+cv2.destroyAllWindows()
