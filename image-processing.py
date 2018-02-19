9dc5279bc3535f1ff06268584a530ed961af8174
sagather <samantha.agather@hotmail.com>
2018-02-18 20:50:50 -0800

    beginning refactoring in image-processing.py from face-test

--- image-processing.py
+++ image-processing.py
@@ -1,95 +1,82 @@
-import args as args
-import cv2;
-import argparse;
-import datetime;
-import imutils;
-import time;
+#All import statements
 
-_faceCascade = cv2.CascadeClassifier('HaarCascades/haarcascade_frontalface_default.xml');
-_eyeCascade = cv2.CascadeClassifier('HaarCascades/haarcascade_eye.xml');
-text = "";
+import cv2
+#for motion detection
+import argparse
+import datetime
+import imutils
+import time
+import numpy as np
 
-def processImages(grayFeed):
-    facesDetected = _faceCascade.detectMultiScale(grayFeed, 1.3, 5);
+#Referenced for motion detection: https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
 
-    #   For loop begin
-    for (x, y, w, h) in facesDetected:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2);
-        roiGray = grayFeed[y:y + (h / 2), x:x + w];
-        roiColor = frame[y:y + h, x:x + w];
+#Might be needed later
+#   _feedWidth = originalFeed.get(3)
+#   _feedHeight = originalFeed.get(4)
 
-        # Detect eyes using haarcascade_eye.xml
-        eyesDetected = _eyeCascade.detectMultiScale(roiGray);
+# Load in cascade files
+_face_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_frontalface_default.xml')
+_eye_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_eye.xml')
+_leftEye_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_lefteye_2splits.xml')
+_rightEye_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_righteye_2splits.xml')
 
-        #   inner for loop begin
-        for (ex, ey, ew, eh) in eyesDetected:
-            cv2.rectangle(roiColor, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 1);
+_originalFeed = cv2.VideoCapture(0)
 
-        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+_ap = argparse.ArgumentParser()
+_ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+_args = vars(_ap.parse_args())
 
-        cv2.imshow("Security Feed", frame)
-        cv2.imshow("Thresh", thresh)
-        cv2.imshow("Frame Delta", frameDelta)
+_firstFrame = None;
+_frameText = ""
 
-        text = "Occupied"
+#TODO:  GET FIRST FRAME FUNCTION
+def firstFrame():
 
-        #inner for loop end
-
-    #   Outer for loop end
-
-    return roiColor;
-
-def makeGrayscale(inputFrame):
-
-    grayscale = cv2.cvtColor(inputFrame, cv2.COLOR_BGR2GRAY);
-    grayscale = cv2.GaussianBlur(grayscale, (21, 21), 0);
-    return grayscale;
-
-def edgeDetection(grayScaleFrame):
-
-    returnVal, edges = cv2.threshold(grayScaleFrame, 100, 200, cv2.THRESH_BINARY);
-    return edges;
+    if _originalFeed.isOpened():  # try to get the first frame
+        rval, frame = _originalFeed.read()
+        _frameText = "No Motion"
+        return rval;
 
 
-_videoCapture = cv2.VideoCapture(0);
+#TODO:  PUT TEXT INTO FRAME
 
-ap = argparse.ArgumentParser();
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size");
-args = vars(ap.parse_args());
 
-#   While loop begin
-while True:
 
-    ret, frame = _videoCapture.read();
-    text = "Neutral";
-    grayscale = makeGrayscale(frame);
+#TODO: FACE DETECTION FUNTCION
 
-    if(firstframe is None):
-        firstframe = grayscale;
-        continue;
 
-    frameDelta = cv2.absdiff(firstframe, grayscale);
+
+#TODO:  MAIN FUNCTION
+
+while rval:
+    rval, frame = _originalFeed.read()
+
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if firstFrame is None:
+        firstFrame = gray
+        continue
+
+    frameDelta = cv2.absdiff(firstFrame, gray)
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
     thresh = cv2.dilate(thresh, None, iterations=2)
-    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                 cv2.CHAIN_APPROX_SIMPLE)
+    (_,cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for c in cnts:
-        if cv2.contourArea(c) < args["min_area"]:
+        if cv2.contourArea(c) < _args["min_area"]:
             continue
 
-    cv2.imshow('face processed', processImages(grayscale));
+        (mx, my, mw, mh) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (mx, my), (mx + mw, my + mh), (244, 66, 232), 2)
+        text = "Motion Detected"
 
-    cv2.imshow('grayscale', grayscale);
+        # draw the text and timestamp on the frame
+        #may not be needed, but may be useful for Sam
+        cv2.putText(frame, "Frame Status: {}".format(text), (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break;
-
-#   while loop end
-
-_videoCapture.release();
-cv2.destroyAllWindows();
