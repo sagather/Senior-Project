9189ca3b786498aac19ddd622389dcb4b7989d4d
mostby <megan.m.ostby@gmail.com>
2018-02-18 19:32:02 -0800

    Got motion detection working, unsure if working with face detection or not, as poor lighting conditions do not allow for face detection to work. Further work is needed to get coordinates to be marked down if motion occurs within them.

--- face-test.py
+++ face-test.py
@@ -1,5 +1,16 @@
 import cv2
+#for motion detection
+import argparse
+import datetime
+import imutils
+import time
 import numpy as np
+
+#Referenced for motion detection: https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
+
+#Might be needed later
+    # feedWidth = originalFeed.get(3)
+    # feedHeight = originalFeed.get(4)
 
 # Load in cascade files
 face_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_frontalface_default.xml')
@@ -10,18 +21,50 @@ rightEye_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_righteye_2spl
 originalFeed = cv2.VideoCapture(0)
 
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
+
+firstFrame = None;
+
 if originalFeed.isOpened():  # try to get the first frame
     rval, frame = originalFeed.read()
-    #feedWidth = originalFeed.get(3)
-    #feedHeight = originalFeed.get(4)
-else:
-    rval = False
+    text = "No Motion"
 
 while rval:
     rval, frame = originalFeed.read()
 
-    grayFeed = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    facesDetected = face_cascade.detectMultiScale(grayFeed, 1.3, 5)
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if firstFrame is None:
+        firstFrame = gray
+        continue
+
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (_,cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in cnts:
+        if cv2.contourArea(c) < args["min_area"]:
+            continue
+
+        (mx, my, mw, mh) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (mx, my), (mx + mw, my + mh), (244, 66, 232), 2)
+        text = "Motion Detected"
+
+        # draw the text and timestamp on the frame
+        #may not be needed, but may be useful for Sam
+        cv2.putText(frame, "Frame Status: {}".format(text), (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+#Face Detection
+    facesDetected = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in facesDetected:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
         #Top
@@ -31,7 +74,7 @@ while rval:
         cv2.rectangle(frame, (x+(w/2),y+(2*h)), (x+(2*w)+w, y+(4*h)), (0, 255,0), 2) #To the left of face as displayed on webcam (Green)
         cv2.rectangle(frame, (x+(w/2),y+(2*h)), (x-(2*w), y+(4*h)), (0, 0,255), 2) #To the right of face as displayed on webcam
 
-        roiGray = grayFeed[y:y+(h/2), x:x+w]
+        roiGray = gray[y:y+(h/2), x:x+w]
         roiColor = frame[y:y+h, x:x+w]
 
         cv2.imshow("roiGray", roiGray);
@@ -42,10 +85,9 @@ while rval:
         for (ex,ey,ew,eh) in eyesDetected:
             cv2.rectangle(roiColor, (ex, ey), (ex+ew, ey+eh), (0,255,0), 1)
 
-
     # blue box = faces, green = eyes, red = left eyes, white = right eyes
     cv2.imshow("preview", frame)
-    cv2.imshow("grayFeed", grayFeed);
+    cv2.imshow("grayFeed", gray);
 
     key = cv2.waitKey(20)
     if key == 27:  # exit on ESC
