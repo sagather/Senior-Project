000e74363b5a41c81e6c2417621efcd9ddd46bd7
Cody Machine <codymachine93@gmail.com>
2018-02-23 21:27:35 -0800

    Merging the code in face-test.py into image-processing.py
    I broke it....

--- image-processing.py
+++ image-processing.py
@@ -15,7 +15,6 @@ import imutils
 
 # Load in cascade files
 _face_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_frontalface_default.xml')
-_eye_cascade = cv2.CascadeClassifier('HaarCascades/haarcascade_eye.xml')
 
 _originalFeed = cv2.VideoCapture(0)
 
@@ -26,6 +25,7 @@ _args = vars(_ap.parse_args())
 _firstFrame = None
 _grayFrame = None
 _frameText = ""
+_motionStateArray = [0, 0, 0, 0]
 
 
 def firstFrame():
@@ -48,6 +48,8 @@ def updateTimestampTextOnFrame():
 
 
 def faceDetect():
+    global _firstFrame
+    global _frameText
     facesDetected = _face_cascade.detectMultiScale(_grayFrame, 1.3, 5)
     for (x, y, w, h) in facesDetected:
         cv2.rectangle(_firstFrame, (x, y), (x+w, y+h), (255, 0, 0), 2)
@@ -63,14 +65,40 @@ def faceDetect():
         # To the right of face as displayed on webcam
         cv2.rectangle(_firstFrame, (x+(w/2), y+(2*h)), (x-(2*w), y+(4*h)), (0, 0, 255), 2)
 
-        # setup roi boxes
-        roiGray = _grayFrame[y:y+(h/2), x:x+w]
-        roiColor = _firstFrame[y:y+h, x:x+w]
+        #
+        (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Detect eyes using haarcascade_eye.xml
-        eyesDetected = _eye_cascade.detectMultiScale(roiGray)
-        for (ex, ey, ew, eh) in eyesDetected:
-            cv2.rectangle(roiColor, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 1)
+        #
+        for c in cnts:
+            if cv2.contourArea(c) < _args["min_area"]:
+                continue
+
+            (mx, my, mw, mh) = cv2.boundingRect(c)
+            cv2.rectangle(_firstFrame, (mx, my), (mx + mw, my + mh), (244, 66, 232), 2)
+
+            # Something Like this to check for specific motion?
+            if (mx >= x-(2*w) and mx <= x+(w/2)) and (my >= y and my <= y+(2*h)):
+                _frameText = "                    Motion Top Right"
+                _motionStateArray[1] = 1
+            elif (mx <= x+(2*w)+w) and (mx >= x+(w/2)) and (my >= y and my <= y+(2*h)):
+                _frameText = "Motion Top Left"
+                _motionStateArray[0] = 1
+            elif (mx >= x-(2*w) and mx <= x+(w/2)) and (my >= y + (2 * h) and my <= y + (4 * h)):
+                _frameText = "                    Motion Bottom Right"
+                _motionStateArray[3] = 1
+            elif (mx <= x+(2*w)+w) and (mx >= x+(w/2)) and (my >= y+(2*h) and my <= y+(4*h)):
+                _frameText = "Motion Bottom Left"
+                _motionStateArray[2] = 1
+
+            if _motionStateArray[0] == 1 and _motionStateArray[1] == 1:
+                _frameText = "    Motion BOTH Top"
+            if _motionStateArray[2] == 1 and _motionStateArray[3] == 1:
+                _frameText = "    Motion BOTH Bottom"
+
+            # Flip frame then add text
+            _firstFrame = cv2.flip(_firstFrame, 1)
+
+
 
 
 def end():
@@ -84,12 +112,20 @@ rval = True
 while rval:
     rval = firstFrame()
 
-    if _firstFrame is None:
-        print("Error: _firstFrame was not set")
-        break
-
     _firstFrame = imutils.resize(_firstFrame, width=500)
     _grayFrame = cv2.cvtColor(_firstFrame, cv2.COLOR_BGR2GRAY)
+    _grayFrame = cv2.GaussianBlur(_grayFrame, (21, 21), 0)
+
+
+    if _firstFrame is None:
+        _firstFrame = _grayFrame
+        continue
+
+    frameDelta = cv2.absdiff(_firstFrame, _grayFrame)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+
+    _motionStateArray = [0, 0, 0, 0]
 
     faceDetect()
     
