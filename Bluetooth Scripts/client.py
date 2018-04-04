037e1880bae54c8bdebfcc286c8b59475b0c3510
sagather <samantha.agather@hotmail.com>
2018-04-03 21:47:51 -0700

    reorganized client script

--- Bluetooth Scripts/client.py
+++ Bluetooth Scripts/client.py
@@ -5,14 +5,24 @@ import bluetooth
 #TODO Change the MAC address to your raspberry pi's mac address
 #TODO easiest way to find that is to run the bluetooth-tester program on your raspberry pi
 
-serverMACAddress = "B8:27:EB:9E:2A:D6"
-port = 3
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((serverMACAddress, port))
-while 1:
-    text = raw_input()
-    if text == "quit":
-        break
-    else:
-        s.send(text)
-s.close()
+class Client():
+
+    global s
+
+    def __init__(self):
+
+        serverMACAddress = "B8:27:EB:9E:2A:D6"
+        port = 3
+        s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        s.connect((serverMACAddress, port))
+
+    def send(self, text):
+        while 1:
+            text = raw_input()
+            if text == "quit":
+                break
+            else:
+                s.send(text)
+
+    def quit(self):
+        s.close()
